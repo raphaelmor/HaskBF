ac4d03170723b3d1d7dc72ca970b742a6da3a8f0
Raphaël Mor <raphael@capitainetrain.com>
2015-10-28 10:53:19 +0100

    Read input from commandline

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,21 @@
 module Main where
+
+import Options.Applicative
+import System.IO
+
 import Interpreter
 
-helloWorld :: [Char]
-helloWorld = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."
-
-cat :: [Char]
-cat = ",[.,]"
-
-fib :: [Char]
-fib = ">++++++++++>+>+[[+++++[>++++++++<-]>.<++++++[>--------<-]+<<<]>.>>[[-]<[>+<-]>>[<<+>+>-]<[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>+<-[>[-]>+>+<<<-[>+<-]]]]]]]]]]]+>>>]<<<]"
-
 main :: IO ()
-main = runBF helloWorld
+main = do
+        filePath <- execParser (parseFile `withInfo` "Interpret a Brainfuck file")
+        fileHandle <- openFile filePath ReadMode
+        contents <- hGetContents fileHandle
+        runBF contents
+
+withInfo :: Parser a -> String -> ParserInfo a
+withInfo opts desc = info (helper <*> opts) $ progDesc desc
+
+parseFile :: Parser FilePath
+parseFile = strOption $
+  short 'f' <> long "file" <> metavar "FILE-PATH" <>
+  help "Path of Brainfuck file to interpret"
